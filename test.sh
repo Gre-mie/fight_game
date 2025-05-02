35e9b8b90fb59622a8e18dc1cf10e005ca326405
Gre-mie <meow31-cat@outlook.com>
2025-05-02 22:16:35 +0100

    Add check if path exists

--- test.sh
+++ test.sh
@@ -8,8 +8,14 @@ tests() {
     if [[ $1 ]]; then
         start="$1"
     fi
-    
-    printf "tests running from $start\n"
+
+    if [[ -e "$start" ]]; then
+        printf "file $start exists\n"                 # TODO: send success messege to log
+    else
+                                                    # TODO: send error messege to log (no colour)
+        trap "Error: $start cannot be found" EER    # TODO: Fix this and add colour to errors if possible
+    fi
+
 }
 
 tests "$1"
