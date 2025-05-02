53e998050fc9643f5994b96c737e84f53f2071a1
Gre-mie <meow31-cat@outlook.com>
2025-05-02 21:59:22 +0100

    Set starting path for DFS

--- test.sh
+++ test.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 # This script will exicute tests within the tests/ directory
+set -e
+
+tests() {
+    local start="./tests"
+    if [[ $1 ]]; then
+        start="$1"
+    fi
+    
+    printf "tests running from $start\n"
+}
+
+tests "$1"
