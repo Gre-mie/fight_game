c48f6e56d9284930b71e75e82e1545d955a1e528
Gre-mie <meow31-cat@outlook.com>
2025-05-11 20:06:55 +0100

    Add function to log messages to log.txt

--- main.sh
+++ main.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 # This script is the entry point for the game
+
+date_time=$(date "+%Y-%m-%d %H:%M:%S\t")
+printf "${date_time}INFO: log.txt cleared\n" > log.txt
+
+# log format <date/time> <message status:> <message> 
+# types of message status  INFO: ERROR:
+
+log() {
+    date_time=$(date "+%Y-%m-%d %H:%M:%S\t")
+    printf "$date_time$1\n" >> log.txt
+}
+
+log "ERROR: bla bla bla"
+
+# TODO: add to contributing
+# move log definition (top of file?)
