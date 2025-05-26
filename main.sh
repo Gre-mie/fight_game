4f6fcdd9f41987d55e05c56b49923adca9892022
Gre-mie <meow31-cat@outlook.com>
2025-05-26 20:57:45 +0100

    Added setup and game loop

--- main.sh
+++ main.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-# load necessary files globally
-. $(find ./ -name "log.sh")
-
 # This script is the entry point for the game
-set -e
 
-log "INFO: main.sh running..."
+# load necessary files globally
+. ./functs/debug/log.sh
+. ./functs/paths/is_path.sh
 
-# Game setup function
+#set -e
+
+# Game setup
+# load global variables
+log "INFO: loading global variables"
+. ./vars/entities/player.sh
+
 
 # Game loop
+running=true
+frame=1
+while [[ $running == true ]]; do
+log "INFO: starting game loop..."
 
-log "INFO: main.sh complete"
+printf "frame: ${frame}\n"
+sleep 0.25
+
+# temp exit condition
+if [[ $frame -ge 3 ]]; then
+running=false
+fi
+
+((frame++))
+done
+
+log "INFO: game loop ended naturally"
+return 0
