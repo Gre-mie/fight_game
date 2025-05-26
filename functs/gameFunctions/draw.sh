5178bdffe0d70156f97f4721859dbf492443e71a
Gre-mie <meow31-cat@outlook.com>
2025-05-26 21:11:48 +0100

    Add game draw logic with notes for game loop

---
+++ functs/gameFunctions/draw.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Draws to the screen
+draw() {
+    clear
+    printf "frame: ${frame}\n"
+    sleep 0.25
+}
--- main.sh
+++ main.sh
@@ -2,32 +2,35 @@
 
 # This script is the entry point for the game
 
-# load necessary files globally
+# Game setup
+# load necessary functions globally
 . ./functs/debug/log.sh
 . ./functs/paths/is_path.sh
+. ./functs/gameFunctions/draw.sh
 
-#set -e
-
-# Game setup
 # load global variables
 log "INFO: loading global variables"
 . ./vars/entities/player.sh
 
 
 # Game loop
+# all game logic for updating
 running=true
 frame=1
 while [[ $running == true ]]; do
 log "INFO: starting game loop..."
 
-printf "frame: ${frame}\n"
-sleep 0.25
+# getting input from user should go here
+
+# update logic should go here
 
 # temp exit condition
-if [[ $frame -ge 3 ]]; then
+if [[ $frame -ge 5 ]]; then
 running=false
 fi
 
+
+draw
 ((frame++))
 done
 
