928440f5e291fe40ae60b0eb91393b74d1d2ec0c
Gre-mie <meow31-cat@outlook.com>
2025-05-27 20:31:39 +0100

    Complete tests for get_input funciton

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@
 #set -e
 
 # set global pass/fail with colour
-col_fail="\033[31m X \033[39m"
-col_pass="\033[92m > \033[39m"
+col_fail="\033[31mX \033[39m"
+col_pass="\033[92m> \033[39m"
 
 run_tests() { # args: string | void -> void
     local path="./tests"
--- tests/testInput/test_get_input.sh
+++ tests/testInput/test_get_input.sh
@@ -2,71 +2,79 @@
 
 . ./functs/game_functions.sh
 
+test_get_input() {
+
+
 # vars to be returned in a string
 test_funct="testing: get_input"
 test_pass=0
 test_fail=0
-
 exit_code=$?
-
-return_str=("${test_funct}: pass: ${test_pass}  fail: ${test_fail}")
+test_return_status=("") # starting the list with an empty string solves spacing issue
 
 
 test_title="test no args"
 get_input 2> err.log
-if [[ $exit_code -ne 0 ]]; then
-    printf "${col_fail}"
+exit_code=$?
+if [[ $exit_code == 0 ]]; then
+    test_return_status+=("${col_fail}${test_title}\n")
 else
     ((test_pass++))
 fi
-printf "${test_title}\n"
 
 
 test_title="test 3 args"
 get_input one two three 2> err.log
-if [[ $exit_code -ne 0 ]]; then
-    printf "${col_fail}"
+exit_code=$?
+if [[ $exit_code == 0 ]]; then
+    test_return_status+=("${col_fail}${test_title}\n")
 else
-    printf "${col_pass}"
+    ((test_pass++))
 fi
-printf "${test_title}\n"
 
 
 test_title="test 1st arg!=int"
 get_input abc 2> err.log
-if [[ $exit_code -ne 0 ]]; then
-    printf "${col_fail}"
+exit_code=$?
+if [[ $exit_code == 0 ]]; then
+    test_return_status+=("${col_fail}${test_title}\n")
 else
-    printf "${col_pass}"
+    ((test_pass++))
 fi
-printf "${test_title}\n"
 
 
 test_title="test both args=int"
 get_input 12 444 2> err.log
-if [[ $exit_code -ne 0 ]]; then
-    printf "${col_fail}"
+exit_code=$?
+if [[ $exit_code == 0 ]]; then
+    test_return_status+=("${col_fail}${test_title}\n")
 else
-    printf "${col_pass}"
+    ((test_pass++))
 fi
-printf "${test_title}\n"
 
 
 test_title="test 1st arg=int 2nd arg=invalid str"
 get_input 12 Bob 2> err.log
-if [[ $exit_code -ne 0 ]]; then
-    printf "${col_fail}"
+exit_code=$?
+if [[ $exit_code == 0 ]]; then
+    test_return_status+=("${col_fail}${test_title}\n")
 else
-    printf "${col_pass}"
+    ((test_pass++))
 fi
-printf "${test_title}\n"
 
 
 test_title="test 1st arg=int 2nd arg=invalid char"
 get_input 12 Bo@ 2> err.log
-if [[ $exit_code -ne 0 ]]; then
-    printf "${col_fail}"
+exit_code=$?
+if [[ $exit_code == 0 ]]; then
+    test_return_status+=("${col_fail}${test_title}\n")
 else
-    printf "${col_pass}"
+    ((test_pass++))
 fi
-printf "${test_title}\n"
+
+echo "    ${test_funct}\n--- pass: ${test_pass}  fail: ${test_fail} ---\n${test_return_status[@]}\n"
+}
+
+
+final_arr=$(test_get_input)
+printf "${final_arr}"
