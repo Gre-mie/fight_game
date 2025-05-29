9324deb29b3d971381dc01419e05ec34646b5737
Gre-mie <meow31-cat@outlook.com>
2025-05-29 10:59:19 +0100

    Add enemies and update docs

--- vars/entities/enemies/orc.sh
+++ vars/entities/enemies/orc.sh
@@ -1,8 +1,8 @@
-# sets enemy variable to a Goblin
+# sets enemy variable to a Orc
 # enemy variables will be reset each time an enemy is spawned to fight
 # only one enemy can be fought at a time
 
-enemy_type="Goblin"
-enemy_health=30
-enemy_defence=3
-enemy_power=5
+enemy_type="Orc"
+enemy_health=35
+enemy_defence=9
+enemy_power=25
