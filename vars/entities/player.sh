a0595e51d0d9ce9fe269e59b26f3b592e94b658a
Gre-mie <meow31-cat@outlook.com>
2025-05-31 14:10:16 +0100

    Add version control variables

--- vars/entities/player.sh
+++ vars/entities/player.sh
@@ -1,6 +1,6 @@
 # set initial player charactor variables
 
-player_name="" # name will be overwriten by another function
+player_name="" # name will be added
 player_health=100
 player_defence=5
 player_power=10
