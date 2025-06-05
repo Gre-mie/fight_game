d18a23bd941857f80dbae296c0f3c11dd53915f1
Gre-mie <meow31-cat@outlook.com>
2025-06-05 12:48:01 +0100

    pull in changes from Release_Battle_update branch

--- vars/global_vars.sh
+++ vars/global_vars.sh
@@ -2,30 +2,13 @@
 # do NOT add variables directly related to the game loop
 
 # window/terminal variables
-gamescreen_min_width=59     # cols
-gamescreen_min_height=23    # lines
 
-
-# exits if the terminal is too small
-log "INFO: checking terminal size"
-
-terminal_width=$(tput cols)
-if [[ $terminal_width -lt $gamescreen_min_width ]]; then
-    log "ERROR: terminal width to small: $terminal_width, min width = $gamescreen_min_width"
-    printf "\033[33mERROR:\033[39m terminal width to small: $terminal_width, min width = $gamescreen_min_width\n"
-    exit 1
-fi
-terminal_height=$(tput lines)
-if [[ $terminal_height -lt $gamescreen_min_height ]]; then
-    log "ERROR: terminal height to small: $terminal_height, min height = $gamescreen_min_height"
-    printf "\033[33mERROR:\033[39m terminal height to small: $terminal_height, min height = $gamescreen_min_height\n"
-    exit 1
-fi
 
 
 # game variables 
-game_version="v0.2.0"
-update="Graphics Update" # this is here mostly to remind myself to think about what a feature is
+game_version="v0.1.1"
+working_on="Graphics Update"
+update="Battle Update" # this is here mostly to remind myself to think about what a feature is
 
 # log version determined by the branch
 if [[ $(git rev-parse --abbrev-ref HEAD) == $release_branch_name ]]; then
