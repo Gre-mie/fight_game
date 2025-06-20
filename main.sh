9033d11c40192713b8e7516aad2c6e2e41b42020
Gre-mie <meow31-cat@outlook.com>
2025-06-20 20:51:21 +0100

    Add colour variables to error messages and cleanup

--- main.sh
+++ main.sh
@@ -2,6 +2,11 @@
 
 # This script is the entry point for the game
 
+# Error segments with colours to be used when printing errors
+err_x_msg_seg="\033[31mX\033[39m"      # used when printing to game screen, programme should continue
+err_msg_seg="\033[33mERROR:\033[39m"   # programme should be exited after using this either with exit or return
+reset_msg_seg="\033[0m\n" # this resets terminal colours to original colours
+
 # Game setup
 # load necessary functions globally
 . ./functs/debug.sh
@@ -26,13 +31,13 @@ fi
  terminal_width=$(tput cols)
  if [[ $terminal_width -lt $gamescreen_min_width ]]; then
      log "ERROR: terminal width to small: $terminal_width, min width = $gamescreen_min_width"
-     printf "\033[33mERROR:\033[39m terminal width to small: $terminal_width, min width = $gamescreen_min_width\n"
+     printf "${err_msg_seg} terminal width to small: $terminal_width, min width = $gamescreen_min_width${reset_msg_seg}"
      exit 1
  fi
  terminal_height=$(tput lines)
  if [[ $terminal_height -lt $gamescreen_min_height ]]; then
      log "ERROR: terminal height to small: $terminal_height, min height = $gamescreen_min_height"
-     printf "\033[33mERROR:\033[39m terminal height to small: $terminal_height, min height = $gamescreen_min_height\n"
+     printf "${err_msg_seg} terminal height to small: $terminal_height, min height = $gamescreen_min_height${reset_msg_seg}"
      exit 1
  fi
 
@@ -41,7 +46,8 @@ fi
 printf "Enter your name: "
 player_name=$(get_input 20 string)
 if [[ ${#player_name} -lt 1 ]]; then
-    log "WARNING: player name was not set"
+    player_name="Anony"
+    log "WARNING: player name was not set, default name: $player_name"
 else
     log "INFO: player name was set to: $player_name"
 fi
@@ -68,7 +74,8 @@ if [[ $next_level -le $max_levels && $current_enemy -eq "${#current_room[@]}" &&
 fi
 # win condition is checked after room and enemy is loaded
 if [[ $next_level -ge $max_levels && $current_enemy -ge "${#current_room[@]}" && $enemy_health -lt 1 ]]; then
-    printf "\nYOU WIN :D\n-> score: $game_score\n" # will call end screen function
+    printf "\nYOU WIN :D\n-> score: $game_score${reset_msg_seg}" # will call end screen function
+    printf "Final Score: ${game_text_yellow}${game_score}${game_text_default}${reset_msg_seg}" # temp code
     log "INFO: no more enemies, player wins"
     running=false
     exit 0
@@ -89,7 +96,7 @@ player_option=$(get_input ${#player_options_arr[@]})
 
 if [[ $player_option -eq 0 ]]; then
     log "WARNING: player ended the game"
-    printf "Final Score: ${game_text_yellow}${game_score}${game_text_default}\n" # will call end screen function
+    printf "Final Score: ${game_text_yellow}${game_score}${game_text_default}${reset_msg_seg}" # will call end screen function
     exit 2
 fi
 
@@ -101,13 +108,14 @@ game_battle
 if [[ $player_health -lt 1 ]]; then
     log "INFO: player has died, health: $player_health"
     printf "You Died :(\n" # will call end screen function
+    printf "Final Score: ${game_text_yellow}${game_score}${game_text_default}${reset_msg_seg}" # temp code
     running=false
+    exit 0
 fi
 
 # log if enemy is dead
 if [[ $enemy_health -lt 1 ]]; then
     log "INFO: ${enemy_type} was slain" # will call funciton that prints in the screen logs area
-    printf "${enemy_type} was slain\n\n" # <<< temp code
 fi
 
 ((frame++))
